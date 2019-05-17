436c13c245ba67c713f0615ab8b28ccdb77b5c55
Stavros <stavros.g@web.de>
2019-05-17 17:07:19 +0200

    fck you

---
+++ FlugzeugTest.py
@@ -0,0 +1,18 @@
+from Flugzeug import Flugzeug
+
+
+# ---------------------------------------- c)-------------------------------------------------------
+class FlugzeugTest():
+
+    @staticmethod
+    def test():
+        testsequenz = [("FG3057P", 301), ("FG3295P", -1), ("F3290P", 56), ("FG4090S", 17), ("FG3200R", 114),
+                       (None, 295)]
+
+        for testfall in testsequenz:
+            flugzeug = Flugzeug(testfall[0], testfall[1])
+
+            print(flugzeug.getFlugzeugnummer() + "  =>  " + str(flugzeug.istFlugzeugGueltig()))
+
+
+FlugzeugTest.test()
